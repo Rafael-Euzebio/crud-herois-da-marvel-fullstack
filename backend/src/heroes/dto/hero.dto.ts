62bd347d63513b4a4c84def3142a68a70a8bf0cb
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-10-23 15:01:01 -0300

    feat: enable validation pipe
    - Heroes sent on body must have:
      - a non empty `name`
      - A non empty array of `abilities`, with at least 3 abilities
      - A non empty description of their `origins`, with at least 20
        characters

--- backend/src/heroes/dto/hero.dto.ts
+++ backend/src/heroes/dto/hero.dto.ts
@@ -1,5 +1,18 @@
+import { ArrayMinSize, IsArray, IsNotEmpty, IsNotEmptyObject, IsString, MinLength, ValidateNested } from "class-validator";
+
 export class HeroDTO {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @ArrayMinSize(3)
+  @IsString({ each: true})
+  @IsNotEmpty({ each: true })
+  @MinLength(3, { each: true })
   abilities: string[];
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(20, { each: true })
   origins: string;
 }
