aa76af7628de70f4210d91c43f736d652281f71d
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-10-27 13:09:44 -0300

    chore: install and setup tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,4 +21,10 @@ export default defineConfig({
       }
     }
   })],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
+  ,
 })
