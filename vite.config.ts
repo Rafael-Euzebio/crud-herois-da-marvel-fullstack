dc44f70e73e2c5a2a07528f407a09fd144daad41
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-10-27 03:16:09 -0300

    chore: add proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''), // This line is important
+      }
+    }
+  },
   plugins: [react({
     babel: {
       parserOpts: {
