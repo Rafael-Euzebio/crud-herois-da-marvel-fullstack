2623103f8a5abfe9c3741be3c5841e5c85b41118
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-10-31 20:45:00 -0300

    feat: make HeroList scrollable on large screens

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import HeroList from "./components/HeroList"
 function App() {
   return (
     <div className="bg-marvel-bg flex">
-      <main className="w-full p-5 flex flex-wrap">
+      <main className="w-full p-5 flex flex-wrap lg:max-h-[935px]">
         <HeroForm />
         <HeroList />
       </main>
