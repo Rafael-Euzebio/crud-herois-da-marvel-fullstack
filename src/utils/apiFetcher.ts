dec87c138dced172abac80e87a5d1c3ce5dcc092
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-10-31 01:50:18 -0300

    refactor: define return type for `apiFetcher` functions

--- src/utils/apiFetcher.ts
+++ src/utils/apiFetcher.ts
@@ -1,9 +1,17 @@
 import axios from "axios"
+import { HeroRequestDto, HeroResponseDto } from "../dto/HeroDto"
 
-const baseUrl = '/api'
+const baseUrl = '/api/'
+const heroesUrl = `${baseUrl}/heroes/`
+
 export default {
-  async getAll() {
-    const response = await axios.get(`${baseUrl}heroes`)
+  async getAll(): Promise<HeroResponseDto[]> {
+    const response = await axios.get(heroesUrl)
     return response.data
+  },
+  async insertOne(hero: HeroRequestDto): Promise<HeroResponseDto> {
+    const response = await axios.post(heroesUrl, hero)
+    return response.data
+  },
   }
 }
