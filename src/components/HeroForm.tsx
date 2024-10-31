199f1b27dde8b99c5ffcad0f85e10bec5311b35e
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-10-31 17:06:37 -0300

    style: remove taliwind variables and apply styles inline

--- src/components/HeroForm.tsx
+++ src/components/HeroForm.tsx
@@ -53,57 +53,48 @@ const HeroForm = () => {
     reset(editValues) 
   }, [editValues])
 
-  const formClasses = 'lg:w-3/5 p-8 bg-marvel-widget-bg rounded-lg'
-  const labelClasses = 'font-bold text-marvel-base'
-  const inputClasses = 'w-full bg-marvel-input placeholder-gray-300 text-marvel-base rounded appearance-none p-2 mb-1 leading-tight focus:outline-none focus:border-b-marvel-accent-red focus:border-b-2'
-  const fieldsetClasses = `flex flex-wrap border border-dashed border-gray-500 p-2 mb-10 mt-10`
-  const abilityWrapperClasses = 'w-full basis-full sm:basis-1/2 p-1'
-  const checkboxClasses = 'accent-marvel-accent-red mr-2'
-  const errorClasses = 'font-semibold pt-5 text-marvel-error'
-
-
   return (
-    <form onSubmit={onSubmit} className={formClasses}>
-      <label className={labelClasses} htmlFor="name">
+    <form onSubmit={onSubmit} className="lg:w-3/5 p-8 bg-marvel-widget-bg rounded-lg">
+      <label className="font-bold text-marvel-base" htmlFor="name">
         Nome
       </label>
       <input 
-        className={inputClasses}
+        className="w-full bg-marvel-input placeholder-gray-300 text-marvel-base rounded appearance-none p-2 mb-1 leading-tight focus:outline-none focus:border-b-marvel-accent-red focus:border-b-2"
         id="name"
         type="text"
         {...register("name")}
         placeholder="Nome"
       />
-      <span className={errorClasses}>{errors.name?.message}</span>
+      <span className="font-semibold pt-5 text-marvel-error">{errors.name?.message}</span>
 
-      <fieldset className={fieldsetClasses}>
-        <legend className={labelClasses}>Habilidades</legend>
+      <fieldset className="flex flex-wrap border border-dashed border-gray-500 p-2 mb-10 mt-10">
+        <legend className="font-bold text-marvel-base">Habilidades</legend>
         { abilities.map((ability): ReactNode => {
           return (
-          <div className={abilityWrapperClasses}>
+          <div className="w-full basis-full sm:basis-1/2 p-1">
             <input 
               type="checkbox"
-              className={checkboxClasses}
+              className="accent-marvel-accent-red mr-2"
               id={ability}
               value={ability}
               {...register("abilities")}
             />
-            <label className={labelClasses} htmlFor={ability}>{ability}</label>
+            <label className="font-bold text-marvel-base" htmlFor={ability}>{ability}</label>
           </div>
           )
         } )}
-          <span className={errorClasses}>{errors.abilities?.message}</span>
+          <span className="font-semibold pt-5 text-marvel-error">{errors.abilities?.message}</span>
       </fieldset>
 
-      <label className={labelClasses}>
+      <label className="font-bold text-marvel-base">
         Origem
       </label>
       <textarea 
-        className={inputClasses}
+        className="w-full bg-marvel-input placeholder-gray-300 text-marvel-base rounded appearance-none p-2 mb-1 leading-tight focus:outline-none focus:border-b-marvel-accent-red focus:border-b-2"
         {...register("origins")} 
         placeholder="Origem"
       />
-      <span className={errorClasses}>{errors.origins?.message}</span>
+      <span className="font-semibold pt-5 text-marvel-error">{errors.origins?.message}</span>
       {
         editMode === true ? (
           <Button type="submit" text="Salvar" color="blue" />
